fe01b1345ea870d2f8de4d6be4252e222aa0620e
Joaquin Cardenas <jcardenas.lie@gmail.com>
2019-11-03 03:32:39 -0300

    Add basic server functionality

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# ROUTES ############################################################
+
+@app.route('/')
+def hello_world():
+    return "Hello Prices"
+
+# RUN ############################################################
+
+if __name__ == '__main__':
+    app.run(
+        debug = True, 
+        use_reloader=False
+        )
